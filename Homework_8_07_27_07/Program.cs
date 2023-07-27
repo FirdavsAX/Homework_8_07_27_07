1c1a8088b6343fa80b00dcdfd4afa9cfdd1eeb0a
FirdavsAX <firdavsjumayev320@gmail.com>
2023-07-27 18:06:55 +0500

    Add project files.

---
+++ Homework_8_07_27_07/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_8_07_27_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
