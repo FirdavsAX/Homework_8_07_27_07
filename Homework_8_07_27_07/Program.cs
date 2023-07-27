18bc6495e700c28560db1e37151334a18b512258
FirdavsAX <firdavsjumayev320@gmail.com>
2023-07-27 18:21:38 +0500

    Xomaki ko'rinish

--- Homework_8_07_27_07/Program.cs
+++ Homework_8_07_27_07/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string str= "Men bugun avtobusda keldimmikin nima deysiz?";
+
+            int position = (FindPosition(str));
+           str =  FindEventMax(str, position);
+            Console.WriteLine(str);
+
+        }
+        static int FindPosition(string str)
+        {
+            int count=0, maxCount = 1, pos = 0;
+            for (int i = 0; i<str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    count = 0 ;
+                }
+                else
+                {
+                    count++;
+                }
+                if(maxCount < count)
+                {
+                    pos = i;
+                    maxCount= count;
+                }
+            }
+            return pos+1;
+        }
+        static string FindEventMax(string str , int pos)
+        {
+            int lengthEvent = 1;
+
+            if(str.Length-3 < pos)
+            {
+                return "-1";
+            }
+
+            for (int i = pos+1; i<str.Length; i++)
+            {
+                if (str[i] != ' ')
+                {
+                    lengthEvent++; 
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return str.Substring(pos, lengthEvent);
         }
     }
 }
